4565e16c082f4a42a5aad2fd698b06e659314f01
Jakub <jakubkuranda@gmail.com>
2016-05-22 19:58:49 +0200

    Idealny splashscreen :)

--- app/src/main/java/pl/dualjack/testownik/MainActivity.java
+++ app/src/main/java/pl/dualjack/testownik/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,16 +17,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_web_view);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setAllowFileAccess(true);
+        webSettings.setBuiltInZoomControls(false);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/demos/pages-multi-page/index.html");
 
         splashscreen = (RelativeLayout) findViewById(R.id.splashscreen);
         splashscreen.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
 
-                Intent nextScreen = new Intent(getApplicationContext(),WebViewActivity.class);
-                startActivity(nextScreen);
-                finish();   // close splashscreen ( main activity )
+                splashscreen.setVisibility(View.INVISIBLE);
 
                 return false;
             }
--- app/src/main/java/pl/dualjack/testownik/items/libraryItem.java
+++
@@ -1,7 +0,0 @@
-package pl.dualjack.testownik.items;
-
-/**
- * Created by Jakub on 2016-05-22.
- */
-public class libraryItem {
-}
