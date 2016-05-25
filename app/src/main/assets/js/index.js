47615b1d93e8ccce90c1c17e2e5eddf7822c3ff5
dualjack <jakubkuranda@gmail.com>
2016-05-25 17:26:27 +0200

    Prototyp listy tematów i zaczątki widoku testu

--- app/src/main/assets/js/index.js
+++ app/src/main/assets/js/index.js
@@ -3,16 +3,43 @@ $(document).ready(function(){
 	var mail;
 	var pass;
 	var pass_md5;
+	var token;
+	var SERVER = "http://88.199.155.137:82/";
 
 	$("button#login").click(function(){
+
+		var url = SERVER+"api/account/loginuser?email="+mail+"&password="+pass_md5;
 
 		mail = $("input#mail").val();
 		pass = $("input#pass").val();
 		pass_md5 = $.md5(pass);
 
 		$.mobile.loading("show");
+		$("#page_login #error").html("");			// wyczyść div z błędem
 
-		//$.mobile.changePage("#page_list");
+		$.getJSON(url).done(function(json){									// udało się
+
+		  	console.log(json);
+				$.mobile.navigate("#page_list");	// przejdź do listy testów
+
+			}).fail(function( jqxhr, textStatus, error ){					// nie udało się
+
+		    var err = textStatus + ", " + error;
+		    $("#page_login #error").html( "Request Failed: " + err );
+
+			}).always(function(){											// zawsze
+
+				$.mobile.loading("hide");
+
+			});
+
+	});
+
+	$("#page_list button#refresh").click(function(){
+
+		$("#message_refresh").hide();
+		$.mobile.loading("show");
+
 	});
 
 	$("#load_tests").click(function(){
