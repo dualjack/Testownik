412eaac18b001e93ae231d09d9ab18d98b32e9d7
Jakub <jakubkuranda@gmail.com>
2016-05-25 03:50:05 +0200

    front-end panel logowania + biblioteka md5

--- app/src/main/assets/js/index.js
+++ app/src/main/assets/js/index.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
 	$("#load_tests").click(function(){
-		$.get("http://88.199.155.137:82/api/account/loginuser?email=rkrawczyszyn@gmail.com&password=912ec803b2ce49e4a541068d495ab570", function(data, status){
-        $("#response").html("Data: " + data + "\nStatus: " + status);
-    },"json");
+		var url = "http://88.199.155.137:82/api/account/loginuser?email=rkrawczyszyn@gmail.com&password=912ec803b2ce49e4a541068d495ab570&callback=?";
+		url = "http://jsonplaceholder.typicode.com/posts/1";
+		url = "http://dualjack.pe.hu/test.php";
+
+		$.ajaxPrefilter( "json script", function( options ) {
+    	options.crossDomain = true;
+		});
+		$.get( url, function( data ) {
+		  $( "#response" ).html( data["title"] );
+			console.log(data);
+		});
+
 	});
+
+	$("#gen_md5").click(function(){
+		var pass = $("#pass").val();
+		$("#md5").html($.md5(pass));
+	});
+
 });
