589f448e8693be9f54dc375866683249ebf9316d
dualjack <jakubkuranda@gmail.com>
2016-05-25 14:37:56 +0200

    dodane obrazki jquery

--- app/src/main/assets/js/index.js
+++ app/src/main/assets/js/index.js
@@ -1,22 +1,30 @@
 $(document).ready(function(){
+
+	var mail;
+	var pass;
+	var pass_md5;
+
+	$("button#login").click(function(){
+
+		mail = $("input#mail").val();
+		pass = $("input#pass").val();
+		pass_md5 = $.md5(pass);
+
+		$.mobile.loading("show");
+
+		//$.mobile.changePage("#page_list");
+	});
+
 	$("#load_tests").click(function(){
 		var url = "http://88.199.155.137:82/api/account/loginuser?email=rkrawczyszyn@gmail.com&password=912ec803b2ce49e4a541068d495ab570&callback=?";
 		url = "http://jsonplaceholder.typicode.com/posts/1";
 		url = "http://dualjack.pe.hu/test.php";
 
-		$.ajaxPrefilter( "json script", function( options ) {
-    	options.crossDomain = true;
-		});
 		$.get( url, function( data ) {
 		  $( "#response" ).html( data["title"] );
 			console.log(data);
 		});
 
-	});
-
-	$("#gen_md5").click(function(){
-		var pass = $("#pass").val();
-		$("#md5").html($.md5(pass));
 	});
 
 });
